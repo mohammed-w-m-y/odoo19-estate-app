0e9bd52c087b5174a4d51a65fa8c156da36aa845
mohammed-w-m-y <Mohammed.w.m.y@gmail.com>
2026-07-08 15:37:08 +0300

    feat: complete chapter 7 relations and optimize views

--- models/custom_model.py
+++ models/custom_model.py
@@ -1,7 +1,6 @@
 from odoo import models, fields
 
-
-# 1. الموديل الجديد الخاص بنوع العقار (مثلاً: شقة، فيلا، مكتب)
+# 1. الموديل الجديد الخاص بنوع العقار (شقة، فيلا...)
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Real Estate Property Type"
@@ -9,7 +8,7 @@ class EstatePropertyType(models.Model):
     name = fields.Char(string="Name", required=True)
 
 
-# 2. الموديل الرئيسي بتاعك بعد إضافة حقل الربط العلاقة
+# 2. الموديل الرئيسي بعد إضافة جميع علاقات شابتر 7
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
@@ -50,5 +49,9 @@ class EstateProperty(models.Model):
         ('canceled', 'Canceled')
     ], string="Status", required=True, copy=False, default='new')
 
-    # ---- الحقل الجديد المطلوب لقفل الشابتر (العلاقة) ----
+    # ---- حقول العلاقات المطلوبة لشابتر 7 ----
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+    seller_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
